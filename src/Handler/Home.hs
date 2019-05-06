7e7162615f412df89d8202596ee43134b7095552
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2019-05-06 23:30:31 +0900

    Calculate Conversion Rate

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -8,12 +8,7 @@ module Handler.Home where
 import Import
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
 import Text.Julius (RawJS (..))
-
--- Define our data that will be used for creating the form.
-data FileForm = FileForm
-    { fileInfo :: FileInfo
-    , fileDescription :: Text
-    }
+import Text.Read (readMaybe)
 
 -- This is a handler function for the GET request method on the HomeR
 -- resource pattern. All of your resource patterns are defined in
@@ -24,30 +19,30 @@ data FileForm = FileForm
 -- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
 getHomeR = do
-    (formWidget, formEnctype) <- generateFormPost sampleForm
-    let submission = Nothing :: Maybe FileForm
-        handlerName = "getHomeR" :: Text
+    visitos0Text <- lookupGetParam "variants[0][visitors]"
+    conversions0Text <- lookupGetParam "variants[0][conversions]"
+    visitos1Text <- lookupGetParam "variants[1][visitors]"
+    conversions1Text <- lookupGetParam "variants[1][conversions]"
+    let conversionRate0 = (/) <$> parseNumber conversions0Text <*> parseNumber visitos0Text
+        conversionRate1 = (/) <$> parseNumber conversions1Text <*> parseNumber visitos1Text
     defaultLayout $ do
-        let (commentFormId, commentTextareaId, commentListId) = commentIds
-        aDomId <- newIdent
         setTitle "AB Tester"
         $(widgetFile "homepage")
 
-sampleForm :: Form FileForm
-sampleForm = renderBootstrap3 BootstrapBasicForm $ FileForm
-    <$> fileAFormReq "Choose a file"
-    <*> areq textField textSettings Nothing
-    -- Add attributes like the placeholder and CSS classes.
-    where textSettings = FieldSettings
-            { fsLabel = "What's on the file?"
-            , fsTooltip = Nothing
-            , fsId = Nothing
-            , fsName = Nothing
-            , fsAttrs =
-                [ ("class", "form-control")
-                , ("placeholder", "File description")
-                ]
-            }
+parseNumber :: Maybe Text -> Maybe Float
+parseNumber maybeText =
+    case maybeText of
+        Just text -> readMaybe $ unpack text
+        Nothing -> Nothing
 
-commentIds :: (Text, Text, Text)
-commentIds = ("js-commentForm", "js-createCommentTextarea", "js-commentList")
+showPercent :: Maybe Float -> Text
+showPercent maybeFloat =
+    case maybeFloat of
+        Just float -> (pack $ show $ (fromIntegral $ ceiling $ float * 10000) / 100) `mappend` "%"
+        Nothing -> "-"
+
+showMaybeText :: Maybe Text -> Text
+showMaybeText maybeText =
+    case maybeText of
+        Just text -> text
+        Nothing -> ""

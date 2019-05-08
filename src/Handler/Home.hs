95ffd9332707b215b27076bfc54eccc302fb495a
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2019-05-09 00:37:24 +0900

    Define Variant record

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -1,14 +1,15 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
+
 module Handler.Home where
 
-import Import
-import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
-import Text.Julius (RawJS (..))
-import Text.Read (readMaybe)
+import           Import
+import           Text.Read             (readMaybe)
+import           Yesod.Form.Bootstrap3 (BootstrapFormLayout (..),
+                                        renderBootstrap3)
 
 -- This is a handler function for the GET request method on the HomeR
 -- resource pattern. All of your resource patterns are defined in
@@ -19,30 +20,51 @@ import Text.Read (readMaybe)
 -- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
 getHomeR = do
-    visitos0Text <- lookupGetParam "variants[0][visitors]"
-    conversions0Text <- lookupGetParam "variants[0][conversions]"
-    visitos1Text <- lookupGetParam "variants[1][visitors]"
-    conversions1Text <- lookupGetParam "variants[1][conversions]"
-    let conversionRate0 = (/) <$> parseNumber conversions0Text <*> parseNumber visitos0Text
-        conversionRate1 = (/) <$> parseNumber conversions1Text <*> parseNumber visitos1Text
-    defaultLayout $ do
-        setTitle "AB Tester"
-        $(widgetFile "homepage")
+  parameters <- reqGetParams <$> getRequest
+  let variant0 = getVariant parameters 0
+      variant1 = getVariant parameters 1
+      conversionRate0 = fmap calculateConversionRate variant0
+      conversionRate1 = fmap calculateConversionRate variant1
+  defaultLayout $ do
+    setTitle "AB Tester"
+    $(widgetFile "homepage")
 
-parseNumber :: Maybe Text -> Maybe Float
-parseNumber maybeText =
-    case maybeText of
-        Just text -> readMaybe $ unpack text
-        Nothing -> Nothing
+parseNumber :: Text -> Maybe Float
+parseNumber text = readMaybe $ unpack text
 
 showPercent :: Maybe Float -> Text
 showPercent maybeFloat =
-    case maybeFloat of
-        Just float -> (pack $ show $ (fromIntegral $ ceiling $ float * 10000) / 100) `mappend` "%"
-        Nothing -> "-"
+  case maybeFloat of
+    Just float -> (pack $ show $ (fromIntegral $ ceiling $ float * 10000) / 100) `mappend` "%"
+    Nothing -> "-"
+
+showMaybeFloat :: Maybe Float -> Text
+showMaybeFloat maybeFloat = showMaybeText $ fmap (pack . show) maybeFloat
 
 showMaybeText :: Maybe Text -> Text
 showMaybeText maybeText =
-    case maybeText of
-        Just text -> text
-        Nothing -> ""
+  case maybeText of
+    Just text -> text
+    Nothing   -> ""
+
+getVariant :: [(Text, Text)] -> Int -> Maybe Variant
+getVariant parameters index = do
+  visitors <- lookupValue parameters "visitors" index >>= parseNumber
+  conversions <- lookupValue parameters "conversions" index >>= parseNumber
+  Just $ Variant visitors conversions
+
+lookupValue :: [(Text, Text)] -> Text -> Int -> Maybe Text
+lookupValue parameters key index = lookup (getKey key index) parameters
+
+getKey :: Text -> Int -> Text
+getKey key index =
+  "variants[" `mappend` (pack $ show index) `mappend` "][" `mappend` key `mappend` "]"
+
+calculateConversionRate :: Variant -> Float
+calculateConversionRate variant = conversions variant / visitors variant
+
+data Variant =
+  Variant
+    { visitors    :: Float
+    , conversions :: Float
+    }
